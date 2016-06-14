56109acd92a5ebb6e264bf8dfc3b5c133d0301a0
bolzai <bolzai>
2016-06-14 18:19:09 +0530

    added necessary files

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+
+  <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Weather scrapper</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid bgPic">
+  <div class="layer">
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+      <div class="col-md-6">
+        <h1 class="hStyle">ONLINE JOURNAL</h1>
+        <p class="pStyle">"A good journal entry- like a good song, or sketch, or photograph- ought to break up the habitual and life away the film that forms over the eye, the finger, the tongue, the heart. A good journal entry ought to be a love letter to the world."</p>
+      </div>
+      <div class="col-md-6">
+        <div class="wrapper">
+          <h2 class=fheadStyle>Sign up now</h2>
+        </div>
+      </div>
+  </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/functions.js"></script>
+</body>
+</html>  
---
+++ js/functions.js
@@ -0,0 +1,5 @@
+ var height= $(window).height()
+       $(".bgPic").css("min-height",height);
+       $(".layer").css("min-height",height);
+
+ 
